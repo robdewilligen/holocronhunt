6a510db475afb134247ec1091f2cfdd3ee7282c6
Rob de Willigen <robdewilligen@outlook.com>
2022-06-16 13:10:33 +0200

    add List items

--- src/screens/MapListScreen.js
+++ src/screens/MapListScreen.js
@@ -1,12 +1,49 @@
 import React from 'react'
+import { Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { ListItem } from 'react-native-elements'
+
+const list = [
+    {
+        id: '1',
+        title: 'title 1',
+    },
+    {
+        id: '2',
+        title: 'title 2',
+    },
+    {
+        id: '3',
+        title: 'title 3',
+    },
+    {
+        id: '4',
+        title: 'title 4',
+    },
+    {
+        id: '5',
+        title: 'title 5',
+    },
+]
 
 const MapListScreen = ({ navigation }) => {
     return(
-        <SafeAreaView forceInset={{ tio: 'always' }}>
-            <Text style={{ fontSize: 50 }}>MapListScreen</Text>
-        </SafeAreaView>
+        <>
+            <SafeAreaView style={ styles.container } forceInset={{ top: 'always' }}>
+                <FlatList 
+                data={ list }
+                keyExtractor={ item => item.id }
+                renderItem={({ item }) => {
+                    return(
+                        <TouchableOpacity>
+                            <ListItem chevron title={ item.title } />
+                        </TouchableOpacity>
+                    )
+                }}
+                />
+            </SafeAreaView>
+        </>
     )
 }
 
@@ -15,5 +52,12 @@ MapListScreen.navigationOptions = {
         <Icon name= "ios-list" color={ tintColor } size={ 30 } />
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 4,
+        padding: 30,
+    }
+})
 
 export default MapListScreen
